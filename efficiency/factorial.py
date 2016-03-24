ba8f1a8529be3fab411052dbfe69c5713ed6d6ef
Anthony Nandaa <anthony.nandaa@andela.com>
2016-03-24 13:31:33 +0300

    day 4 work

---
+++ efficiency/factorial.py
@@ -0,0 +1,35 @@
+
+
+# args
+
+def factorial(n):
+
+    prod = 1
+
+    for i in range(1, n + 1):
+        prod = prod *  i
+        # return
+
+    return prod
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
